a42405b989ac46b9490773ebdc107b9f48bdbac9
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-07 20:55:09 +0200

    Moved Octokit authentcation file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 const { Octokit, App } = require("octokit");
-const octokitAuth = require('./octokit-auth.json')
+const octokitAuth = require('./data/octokit-auth.json')
 const octokit = new Octokit(octokitAuth);
