48c3833d8d7ba83a5d94387edadbfd401dcf9c87
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-07 20:37:56 +0200

    Added authentication for Octokit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const express = require('express')
 const app = express()
 const { Octokit, App } = require("octokit");
+const octokitAuth = require('./octokit-auth.json')
+const octokit = new Octokit(octokitAuth);
